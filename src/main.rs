886fe57238c34b1e640e7fe9ed62a56f90d4d463
Joe Joyce <joe@decafjoe.com>
2017-12-07 19:19:35 -0500

    Adds an argument for specifying config path(s).

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #[macro_use]
 extern crate clap;
 
-use clap::App;
+use clap::{App, Arg};
+
+const CONFIG_ARG: &str = "CONFIG";
 
 fn main() {
-    App::new("mgit")
+    let m = App::new("mgit")
         .version(crate_version!())
         .author(crate_authors!())
         .about("Small program for managing multiple git repositories.")
+        .arg(Arg::with_name(CONFIG_ARG)
+             .help("Path to configuration file or directory")
+             .short("c")
+             .long("config")
+             .multiple(true)
+             .number_of_values(1)
+             .value_name("PATH"))
         .get_matches();
+
+    let paths: Vec<&str> = match m.values_of(CONFIG_ARG) {
+        Some(paths) => paths.collect(),
+        None => vec!["~/.mgit"],
+    };
 }
