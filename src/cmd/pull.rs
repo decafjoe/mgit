ecdc0e2fb90b663dd6c214e7cccdf76dbb52d62f
Joe Joyce <joe@decafjoe.com>
2017-12-12 00:12:38 -0500

    Moves about messages into subcommand().

--- src/cmd/pull.rs
+++ src/cmd/pull.rs
@@ -3,9 +3,9 @@ use clap::{App, ArgMatches, SubCommand};
 use cfg::Config;
 
 pub const NAME: &str = "pull";
-pub const ABOUT: &str = "Fetch remotes, move tracking refs forward if safe";
 
 pub fn subcommand<'a>() -> App<'a, 'a> {
-    SubCommand::with_name(NAME).about(ABOUT)
+    SubCommand::with_name(NAME)
+        .about("Fetches remotes, move tracking refs forward if safe")
 }
 pub fn run(_: &Config, _: &ArgMatches) {}
--- src/cmd/status.rs
+++ src/cmd/status.rs
@@ -3,14 +3,13 @@ use clap::{App, Arg, ArgMatches, SubCommand};
 use cfg::Config;
 
 pub const NAME: &str = "status";
-pub const ABOUT: &str = "Print status summary for each repo";
 
 const GROUP_ARG: &str = "GROUP";
 const VERBOSE_ARG: &str = "VERBOSE";
 
 pub fn subcommand<'a>() -> App<'a, 'a> {
     SubCommand::with_name(NAME)
-        .about(ABOUT)
+        .about("Prints status summary for each repo")
         .arg(Arg::with_name(GROUP_ARG)
             .help("Separates output by group (default is a merged list)")
             .short("g")
