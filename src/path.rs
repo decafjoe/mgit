2a895bd901961e839808fc90a023c447463427e1
Joe Joyce <joe@decafjoe.com>
2017-12-22 22:09:26 -0500

    Implements display trait for errors.

--- src/path.rs
+++ src/path.rs
@@ -1,5 +1,6 @@
 //! Filesystem path utilities.
 
+use std::fmt;
 use std::path::{MAIN_SEPARATOR, PathBuf};
 
 use users::{get_current_uid, get_user_by_name, get_user_by_uid};
@@ -17,6 +18,12 @@ impl Error {
     /// `message`.
     pub fn new(message: &str) -> Error {
         Error{ message: message.to_owned() }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
     }
 }
 
