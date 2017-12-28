e02d62afb1837150fb3e50fecf2d48d94b8fe0f1
Joe Joyce <joe@decafjoe.com>
2017-12-28 14:57:25 -0500

    Makes clippy and rustfmt happy.
    Note that both tools must be installed out-of-band; they are
    not formally part of the mgit development environment.

--- src/cmd/config.rs
+++ src/cmd/config.rs
@@ -7,26 +7,36 @@ use ordermap::OrderMap;
 use config::ReposIterator;
 use invocation::Invocation;
 
+/// Name of the command (`config`).
 pub const NAME: &str = "config";
 
+/// Name of the argument for tags.
 const TAG_ARG: &str = "TAG";
+/// Name of the argument for verbose output.
 const VERBOSE_ARG: &str = "VERBOSE";
 
+/// Returns configured subcommand instance for this command.
 pub fn subcommand<'a>() -> App<'a, 'a> {
     SubCommand::with_name(NAME)
         .about("Prints configuration values")
-        .arg(Arg::with_name(TAG_ARG)
-             .help("Limits display to repos with specified tag(s)")
-             .short("t")
-             .long("tag")
-             .multiple(true)
-             .number_of_values(1))
-        .arg(Arg::with_name(VERBOSE_ARG)
-             .help("Shows defaults in addition to user-specified config")
-             .short("v")
-             .long("verbose"))
+        .arg(
+            Arg::with_name(TAG_ARG)
+                .help("Limits display to repos with specified tag(s)")
+                .short("t")
+                .long("tag")
+                .multiple(true)
+                .number_of_values(1),
+        )
+        .arg(
+            Arg::with_name(VERBOSE_ARG)
+                .help("Shows defaults in addition to user-specified config")
+                .short("v")
+                .long("verbose"),
+        )
 }
 
+/// Prints the configuration per the arguments specified by the user.
+#[cfg_attr(feature = "cargo-clippy", allow(print_stdout))]
 pub fn run(invocation: &Invocation) {
     if let Some(tags) = invocation.matches().values_of(TAG_ARG) {
         let style = Style::new().bold().underline();
@@ -41,6 +51,8 @@ pub fn run(invocation: &Invocation) {
     println!();
 }
 
+/// Prints the repos in `repos`.
+#[cfg_attr(feature = "cargo-clippy", allow(print_stdout))]
 pub fn print_repos(invocation: &Invocation, repos: ReposIterator) {
     let verbose = invocation.matches().is_present(VERBOSE_ARG);
 
@@ -53,7 +65,9 @@ pub fn print_repos(invocation: &Invocation, repos: ReposIterator) {
     paths.sort();
 
     for path in paths {
-        let repo = invocation.config().repo(path)
+        let repo = invocation
+            .config()
+            .repo(path)
             .expect(&format!("could not get repo for path {}", path));
 
         // Buffer information into a hashmap (which iterates in
@@ -67,26 +81,38 @@ pub fn print_repos(invocation: &Invocation, repos: ReposIterator) {
         // processing the configuration, including checking that the
         // path can be resolved, exists, and can be turned into a
         // string.
-        let path = repo.absolute_path().unwrap().to_str().unwrap().to_owned();
+        let path = repo.absolute_path()
+            .expect("failed to get path for repo")
+            .to_str()
+            .expect("failed to cast path to string")
+            .to_owned();
         facts.insert("path", path);
 
         match repo.name() {
-            Some(name) => { facts.insert("name", name); },
+            Some(name) => {
+                facts.insert("name", name);
+            }
             None => if verbose {
-                facts.insert("name",
-                             format!("{} (default)", repo.name_or_default()));
+                facts.insert(
+                    "name",
+                    format!("{} (default)", repo.name_or_default()),
+                );
             },
         }
 
         match repo.comment() {
-            Some(comment) => { facts.insert("comment", comment); },
+            Some(comment) => {
+                facts.insert("comment", comment);
+            }
             None => if verbose {
                 facts.insert("comment", "<not set>".to_owned());
             },
         }
 
         match repo.symbol() {
-            Some(symbol) => { facts.insert("symbol", symbol); },
+            Some(symbol) => {
+                facts.insert("symbol", symbol);
+            }
             None => if verbose {
                 let symbol = format!("{} (default)", repo.symbol_or_default());
                 facts.insert("symbol", symbol);
@@ -94,13 +120,13 @@ pub fn print_repos(invocation: &Invocation, repos: ReposIterator) {
         }
 
         let tags = repo.tags();
-        if tags.len() > 0 {
+        if !tags.is_empty() {
             let mut s = String::new();
             for (i, tag) in tags.iter().enumerate() {
                 if i != 0 {
                     s.push_str(", ")
                 }
-                s.push_str(&tag)
+                s.push_str(tag)
             }
             facts.insert("tags", s);
         } else if verbose {
@@ -109,18 +135,21 @@ pub fn print_repos(invocation: &Invocation, repos: ReposIterator) {
 
         println!("{}", Purple.bold().paint(repo.path()));
         for (i, (key, value)) in facts.iter().enumerate() {
-            let mut line = String::from("─");
+            let mut line = String::from("\u{2500}");
             for _ in 0..7 - key.len() {
-                line.push_str("─");
+                line.push_str("\u{2500}");
             }
             line.push_str(" ");
             let left = if i == (facts.len() - 1) {
-                "┖"
+                "\u{2516}"
             } else {
-                "┠"
+                "\u{2520}"
             };
-            println!("{}{}",
-                     Blue.paint(format!("  {}{}{}: ", left, line, key)), value)
+            println!(
+                "{}{}",
+                Blue.paint(format!("  {}{}{}: ", left, line, key)),
+                value
+            )
         }
     }
 }
