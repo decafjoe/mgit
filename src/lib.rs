2fa410ddc35737a70385735a6f7ad13e26ecab09
Joe Joyce <joe@decafjoe.com>
2018-01-07 13:12:49 -0500

    Basic code structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
 //! Library that drives mgit.
+#[macro_use]
+extern crate clap;
+
+mod app;
+mod cmd;
 
 /// Entry point for the program.
-#[cfg_attr(feature = "cargo-clippy", allow(print_stdout))]
 pub fn main() {
-    println!("Hello, world!");
+    let matches = app::app()
+        .subcommand(cmd::config::subcommand())
+        .subcommand(cmd::pull::subcommand())
+        .subcommand(cmd::status::subcommand())
+        .get_matches();
+
+    let control = app::run(&matches);
+
+    if matches.subcommand_matches(cmd::config::NAME).is_some() {
+        cmd::config::run(&app::Invocation::new(&control));
+    } else if matches.subcommand_matches(cmd::pull::NAME).is_some() {
+        cmd::pull::run(&app::Invocation::new(&control));
+    } else if matches.subcommand_matches(cmd::status::NAME).is_some() {
+        cmd::status::run(&app::Invocation::new(&control));
+    } else {
+        control.fatal("no command supplied, see `mgit -h` for usage info");
+    }
 }
